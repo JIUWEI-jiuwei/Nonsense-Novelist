7ddd056b12cf54652d0f993a6d78365d127ac703
XysDys <2630589401@qq.com>
2022-06-18 10:44:45 +0800

    0

--- Assets/Scripts/Word/ConcreteClass/Character/LinDaiYu.cs
+++ Assets/Scripts/Word/ConcreteClass/Character/LinDaiYu.cs
@@ -40,6 +40,17 @@ using UnityEngine;
         mainSort = MainSortEnum.psy;
     }
 
+    public override void CreateBullet(GameObject aimChara)
+    {
+        base.CreateBullet(aimChara);
+        DanDao danDao = bullet.GetComponent<DanDao>();
+        danDao.aim = aimChara;
+        danDao.bulletSpeed = 0.5f;
+        danDao.birthTransform = this.transform;
+        ARPGDemo.Common.GameObjectPool.instance.CreateObject(bullet.gameObject.name, bullet.gameObject, this.transform.position, aimChara.transform.rotation);
+
+    }
+
     public override string CriticalText(AbstractCharacter otherChara)
     {
         if (otherChara != null)
